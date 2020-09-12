633c667e13318dac8491e7c35fa3443d0997284c
Shaili Manda <60725976+shailim@users.noreply.github.com>
2020-09-12 10:56:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // boilerplate code for accessing an api
 
+let query = ""; 
+let url = `https://api.spotify.com/v1/search?query=${query}&type=artist&market=US&offset=5&limit=10`
 fetch(url)
 .then(response => response.json()) //unpacking the response into .json format
 .then(function(){
